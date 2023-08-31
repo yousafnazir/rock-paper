740728ed9fbb1b563bf994e027642d88b2280d89
Yousaf Nazir <yousaf.nazir2023@gmail.com>
2023-08-31 17:53:51 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let variable1 = 2;
+// console.log(variable1)
+
+// let calculation = 44 + 6;
+// console.log(calculation)
+// console.log(calculation +200)
+
+
+// let word = 'hello' ;
+// let word1 = ' how are you';
+// console.log( word + word1)
